4a961e29a6925255fec1c79d1c25356bd25839bb
Eithan ASSOULINE <eassouli@e2r1p6.clusters.42paris.fr>
2021-12-20 15:21:29 +0100

    real cpp stuff with std string and toupper

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,28 +6,30 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 18:02:25 by eassouli          #+#    #+#             */
-/*   Updated: 2021/12/20 14:57:25 by eassouli         ###   ########.fr       */
+/*   Updated: 2021/12/20 15:19:54 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <iostream>
-# include <stdio.h>
-# include <ctype.h>
+# include <string>
+# include <locale>
 
-void	toupper_str(char *str) {
+void	toupper_str(std::string str) {
 
-	for (int i = 0; str[i]; i++)
-		putchar(toupper(str[i]));
+	std::locale	loc;
+
+	for ( std::string::size_type i = 0; i < str.length(); ++i )
+		std::cout << std::toupper( str[i], loc );
 }
 
 int		main(int ac, char **av) {
 	
-	if (ac == 1)
+	if ( ac == 1 )
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
-		for (int i = 1; i < ac; i++)
-			toupper_str(av[i]);
+		for ( int i = 1; i < ac; ++i )
+			toupper_str( av[i] );
 		std::cout << std::endl;
 	}
 	return 0;
