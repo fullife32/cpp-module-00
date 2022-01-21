3e24198609d00467d78974fec01bbf1b94063ac0
fullife32 <eithan.assouline6@gmail.com>
2022-01-21 19:52:01 +0100

    display address in 3 ways and content in 2 ways

--- cpp-01/ex02/main.cpp
+++ cpp-01/ex02/main.cpp
@@ -6,13 +6,25 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 18:12:36 by eassouli          #+#    #+#             */
-/*   Updated: 2022/01/21 19:37:01 by eassouli         ###   ########.fr       */
+/*   Updated: 2022/01/21 19:50:25 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
+#include <iostream>
 
 int	main() {
-	std::string	str = "HI THIS IS BRAIN";
+	std::string		string = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &string;
+	std::string&	stringREF = string;
+
+	std::cout << "address of the string :" << std::endl;
+	std::cout << "- in memory :        " << &string << std::endl;
+	std::cout << "- by the pointer :   " << stringPTR << std::endl;
+	std::cout << "- by the reference : " << &stringREF << std::endl << std::endl;
+
+	std::cout << "content of the string :" << std::endl;
+	std::cout << "- by the pointer   : " << *stringPTR << std::endl;
+	std::cout << "- by the reference : " << stringREF << std::endl;
 	return 0;
 }
