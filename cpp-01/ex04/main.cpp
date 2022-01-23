ea7949577ca74383f577245536bc68c78643bd76
fullife32 <eithan.assouline6@gmail.com>
2022-01-23 01:34:49 +0100

    done ex04 and fix with 2 successif chars

--- cpp-01/ex04/main.cpp
+++ cpp-01/ex04/main.cpp
@@ -6,31 +6,68 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 19:57:10 by eassouli          #+#    #+#             */
-/*   Updated: 2022/01/21 21:29:19 by eassouli         ###   ########.fr       */
+/*   Updated: 2022/01/23 01:32:28 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Weapon.hpp"
-#include "HumanA.hpp"
-#include "HumanB.hpp"
+#include <iostream>
+#include <string>
+#include <fstream>
 
-int	main() {
+std::string	replaceLine(std::string str, std::string s1, std::string s2) {
+	std::string	cmp;
+	int			i = 0;
+
+	while (str[i])
 	{
-		Weapon	club = Weapon("crude spiked club");
-		
-		HumanA bob("Bob", club);
-		bob.attack();
-		club.setType("some other type of club");
-		bob.attack();
+		if (str[i] == s1[0])
+		{
+			cmp.assign(str, i, s1.size());
+			if (cmp == s1)
+			{
+				str.erase(i, s1.size());
+				str.insert(i, s2);
+				i+= s2.size() - 1;
+			}
+		}
+		i++;
 	}
+	return (str);
+}
+
+std::string	getExtension( std::string fileName ) {
+	std::string	extension = ".replace";
+	return (fileName + extension);
+}
+
+int	main(int ac, char* av[]) {
+	std::string	dst;
+	if (ac != 4)
 	{
-		Weapon	club = Weapon("crude spiked club");
-		
-		HumanB jim("Jim");
-		jim.setWeapon(club);
-		jim.attack();
-		club.setType("some other type of club");
-		jim.attack();
+		std::cerr << "Error: There must be 3 arguments" << std::endl;
+		return 1;
 	}
+	else if (strlen(av[2]) == 0 || strlen(av[3]) == 0)
+	{
+		std::cerr << "Error: Both strings must not be empty" << std::endl;
+		return 1;
+	}
+	std::ifstream	ifs(av[1]);
+
+	if (!ifs)
+	{
+		std::cerr << "Error: " << av[1] << ": no such file" << std::endl;
+		return 1;
+	}
+	std::ofstream	ofs(getExtension(av[1]));
+	
+	while (!ifs.eof() && !ifs.fail())
+	{
+		std::getline(ifs, dst);
+		ofs << replaceLine(dst, av[2], av[3]) << std::endl;
+	}
+
+	ifs.close();
+	ofs.close();
 	return 0;
 }
