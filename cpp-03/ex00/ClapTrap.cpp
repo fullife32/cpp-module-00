22fdf1f8840969295245465cad5dc218d6ac40c9
Eithan ASSOULINE <eassouli@e2r9p7.clusters.42paris.fr>
2022-02-02 18:27:20 +0100

    Update functions

--- cpp-03/ex00/ClapTrap.cpp
+++ cpp-03/ex00/ClapTrap.cpp
@@ -6,21 +6,46 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 20:26:33 by eassouli          #+#    #+#             */
-/*   Updated: 2022/02/01 14:41:50 by eassouli         ###   ########.fr       */
+/*   Updated: 2022/02/02 17:44:47 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap() { }
+ClapTrap::ClapTrap() {
+	std::cout << "Default Constructor Called" << std::endl;
+}
 
-ClapTrap::~ClapTrap() { }
+ClapTrap::~ClapTrap() {
+	std::cout << "Default Destructor Called" << std::endl;
+}
 
-ClapTrap::ClapTrap( ClapTrap const &other ) { } //set variables here
+ClapTrap::ClapTrap( ClapTrap const &other ) {
+	std::cout << "Copy Constructor Called" << std::endl;
+	*this = other;
+}
 
 ClapTrap &ClapTrap::operator=( ClapTrap const &other ) {
+	std::cout << "Assignment Operator Called" << std::endl;
 	if ( this != &other ) {
-		// set variables here
+		_name = other._name;
+		_hitPts = other._hitPts;
+		_energyPts = other._energyPts;
+		_attackDmg = other._attackDmg;
 	}
 	return *this;
+}
+
+void	ClapTrap::attack( const std::string &target ) {
+	if (this->_hitPts <= 0) {
+		std::cout << this->_name << " has no more HP, attack canceled" << std::endl;
+	}
+}
+
+void	ClapTrap::takeDamage( unsigned int amount ) {
+	
+}
+
+void	ClapTrap::beRepaired( unsigned int amount ) {
+	
 }
--- cpp-03/ex00/ClapTrap.hpp
+++ cpp-03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 20:24:34 by eassouli          #+#    #+#             */
-/*   Updated: 2022/02/01 16:05:08 by eassouli         ###   ########.fr       */
+/*   Updated: 2022/02/02 17:14:55 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,18 @@
 class ClapTrap {
 private:
 	std::string	_name;
-	std::string	_name;
+	int			_hitPts;
+	int			_energyPts;
+	int			_attackDmg;
 public:
 	ClapTrap();
 	~ClapTrap();
 	ClapTrap( ClapTrap const &other );
-	ClapTrap &operator=( ClapTrap const &other );	
+	ClapTrap &operator=( ClapTrap const &other );
+
+	void	attack( const std::string &target );
+	void	takeDamage( unsigned int amount );
+	void	beRepaired( unsigned int amount );
 };
 
 #endif
