129b49e0aa4e482e99c6a97cdc6475673e53de60
Eithan ASSOULINE <eassouli@e2r11p9.clusters.42paris.fr>
2022-02-02 14:06:33 +0100

    add test and use only cpp, with c_str(), find() and replace()

--- cpp-01/ex04/main.cpp
+++ cpp-01/ex04/main.cpp
@@ -6,52 +6,50 @@
 /*   By: eassouli <eassouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 19:57:10 by eassouli          #+#    #+#             */
-/*   Updated: 2022/01/23 01:32:28 by eassouli         ###   ########.fr       */
+/*   Updated: 2022/02/02 14:05:20 by eassouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <fstream>
 
 std::string	replaceLine(std::string str, std::string s1, std::string s2) {
-	std::string	cmp;
-	int			i = 0;
+	std::size_t found = str.find(s1);
 
-	while (str[i])
+	while (found != std::string::npos)
 	{
-		if (str[i] == s1[0])
-		{
-			cmp.assign(str, i, s1.size());
-			if (cmp == s1)
-			{
-				str.erase(i, s1.size());
-				str.insert(i, s2);
-				i+= s2.size() - 1;
-			}
-		}
-		i++;
+		str.replace(found, s1.length(), s2);
+		found = str.find(s1.c_str(), found + s2.length(), s1.length());
 	}
 	return (str);
 }
 
 std::string	getExtension( std::string fileName ) {
-	std::string	extension = ".replace";
-	return (fileName + extension);
+
+	fileName.append(".replace");
+	return fileName;
 }
 
 int	main(int ac, char* av[]) {
 	std::string	dst;
+
 	if (ac != 4)
 	{
 		std::cerr << "Error: There must be 3 arguments" << std::endl;
 		return 1;
 	}
-	else if (strlen(av[2]) == 0 || strlen(av[3]) == 0)
+
+	std::string	original = av[2];
+	std::string	replace = av[3];
+	
+	if (original.length() == 0 || replace.length() == 0)
 	{
 		std::cerr << "Error: Both strings must not be empty" << std::endl;
 		return 1;
 	}
+
 	std::ifstream	ifs(av[1]);
 
 	if (!ifs)
@@ -59,15 +57,15 @@ int	main(int ac, char* av[]) {
 		std::cerr << "Error: " << av[1] << ": no such file" << std::endl;
 		return 1;
 	}
-	std::ofstream	ofs(getExtension(av[1]));
+
+	std::ofstream	ofs(getExtension(av[1]).c_str());
 	
 	while (!ifs.eof() && !ifs.fail())
 	{
 		std::getline(ifs, dst);
-		ofs << replaceLine(dst, av[2], av[3]) << std::endl;
+		ofs << replaceLine(dst, original, replace) << std::endl;
 	}
-
-	ifs.close();
 	ofs.close();
+	ifs.close();
 	return 0;
 }
